5305f3344460100c131f3d0651e3e0aeb6ff8fd6
Haenara <haenara@ubpay.com>
2019-10-14 21:17:50 +0900

    Bump version gradle:3.5.0, compileSdk 28 and migrate to androidx

--- app/src/main/java/com/vansuita/checknewappversionavailable/sample/MainActivity.java
+++ app/src/main/java/com/vansuita/checknewappversionavailable/sample/MainActivity.java
@@ -1,10 +1,10 @@
 package com.vansuita.checknewappversionavailable.sample;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import com.vansuita.checknewappversionavailable.CheckNewAppVersion;
 
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
